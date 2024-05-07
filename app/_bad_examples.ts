48ca59eda7df5d0290148feab1329fbeea3f7fae
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-05-07 15:26:41 -0700

    moved puzzles that had more than 4 in some categories to other file, changed formatting

---
+++ app/_bad_examples.ts
@@ -0,0 +1,70 @@
+import { Category } from "./_types";
+
+export const categoriesList: Category[][] = [
+[
+    {
+      category: "SQUARE NUMBERS",
+      items: ["36", "49", "64", "81"],
+      level: 1,
+    },
+    {
+      category: "PRIME NUMBERS",
+      items: ["41", "43", "47", "53"],
+      level: 2,
+    },
+    {
+      category: "FIBONACCI NUMBERS",
+      items: ["34", "55", "89", "144"],
+      level: 3,
+    },
+    {
+      category: "CATALAN NUMBERS",
+      items: ["5", "14", "42", "132"],
+      level: 4,
+    },
+  ],
+  [
+    {
+      category: "SQUARE NUMBERS",
+      items: ["1", "4", "9", "16"],
+      level: 1,
+    },
+    {
+      category: "DIVISIBLE BY 2 BUT NOT DIVISIBLE BY 3",
+      items: ["2", "8", "10", "14"],
+      level: 2,
+    },
+    {
+      category: "DIVISIBLE BY 3",
+      items: ["3", "6", "12", "15"],
+      level: 3,
+    },
+    {
+      category: "PRIME NUMBERS",
+      items: ["5", "7", "11", "13"],
+      level: 4,
+    },
+  ],
+  [
+    {
+      category: "SQUARE NUMBERS",
+      items: ["100", "10000", "4", "1"],
+      level: 1,
+    },
+    {
+      category: "PRIME NUMBERS",
+      items: ["11", "31", "41", "5"],
+      level: 2,
+    },
+    {
+      category: "DIGITS SUM TO 3",
+      items: ["21", "30", "12", "3"],
+      level: 3,
+    },
+    {
+      category: "MULTIPLES OF 5",
+      items: ["25", "10", "125", "625"],
+      level: 4,
+    },
+  ]
+]
--- app/_examples.ts
+++ app/_examples.ts
@@ -2,165 +2,98 @@ import { Category } from "./_types";
 
 export const categoriesList: Category[][] = [
   [
-  {
-    category: "FIRST DIGITS OF FAMOUS CONSTANTS",
-    items: ["1414", "1618", "2718", "3141"],
-    level: 3,
-  },
-  {
-    category: "PERFECT CUBES",
-    items: ["1000", "1331", "1728", "2197"],
-    level: 1,
-    
-  },
-  {
-    category: "MULTIPLES OF 239",
-    items: ["1434", "1673", "1912", "2151"],
-    level: 2,
-  },
-  {
-    
-    category: "CARMICHAEL NUMBERS",
-    items: ["1105", "1729", "2465", "2821"],
-    level: 4,
-  }
-],
+    {
+      category: "FIRST DIGITS OF FAMOUS CONSTANTS",
+      items: ["1414", "1618", "2718", "3141"],
+      level: 3,
+    },
+    {
+      category: "PERFECT CUBES",
+      items: ["1000", "1331", "1728", "2197"],
+      level: 1,
+
+    },
+    {
+      category: "MULTIPLES OF 239",
+      items: ["1434", "1673", "1912", "2151"],
+      level: 2,
+    },
+    {
+
+      category: "CARMICHAEL NUMBERS",
+      items: ["1105", "1729", "2465", "2821"],
+      level: 4,
+    }
+  ],
   [
-  {
-    category: "AUTO COMPANIES",
-    items: ["AUDI", "BMW", "MERCEDES", "VW"],
-    level: 1,
-  },
-  {
-    category: "UNITS IN PHYSICS",
-    items: ["FARAD", "GAUSS", "HENRY", "TESLA"],
-    level: 2,
-    
-  },
-  {
-    category: "TYPES OF RADIATION",
-    items: ["GAMMA", "IR", "UV", "X"],
-    level: 3,
-  },
-  {
-    
-    category: "FAMOUS FUNCTIONS",
-    items: ["DELTA", "FLOOR", "TOTIENT", "ZETA"],
-    level: 4,
-  }
-],
-  
+    {
+      category: "AUTO COMPANIES",
+      items: ["AUDI", "BMW", "MERCEDES", "VW"],
+      level: 1,
+    },
+    {
+      category: "UNITS IN PHYSICS",
+      items: ["FARAD", "GAUSS", "HENRY", "TESLA"],
+      level: 2,
+
+    },
+    {
+      category: "TYPES OF RADIATION",
+      items: ["GAMMA", "IR", "UV", "X"],
+      level: 3,
+    },
+    {
+
+      category: "FAMOUS FUNCTIONS",
+      items: ["DELTA", "FLOOR", "TOTIENT", "ZETA"],
+      level: 4,
+    }
+  ],
+
   [
-  {
-    category: "PALINDROMES",
-    items: ["121", "11", "434", "797"],
-    level: 1,
-  },
-  {
-    category: "PERFECT CUBES",
-    items: ["1", "8", "27", "64"],
-    level: 2,
-    
-  },
-  {
-    category: "LAST DIGITS OF RECENT YEARS",
-    items: ["20", "21", "22", "23"],
-    level: 3,
-  },
-  {
-    
-    category: "UNLUCKY NUMBERS",
-    items: ["4", "13", "17", "666"],
-    level: 4,
-  }
-],
+    {
+      category: "PALINDROMES",
+      items: ["121", "11", "434", "797"],
+      level: 1,
+    },
+    {
+      category: "PERFECT CUBES",
+      items: ["1", "8", "27", "64"],
+      level: 2,
+
+    },
+    {
+      category: "LAST DIGITS OF RECENT YEARS",
+      items: ["20", "21", "22", "23"],
+      level: 3,
+    },
+    {
+
+      category: "UNLUCKY NUMBERS",
+      items: ["4", "13", "17", "666"],
+      level: 4,
+    }
+  ],
   [
-  {
-    category: "NUMBERS READING THE SAME UPSIDE DOWN",
-    items: ["11", "69", "96", "888"],
-    level: 1,
-  },
-  {
-    category: "CENTRAL BINOMIAL COEFFICIENTS",
-    items: ["20", "70", "252", "924"],
-    level: 4,
-  },
-  {
-    category: "LEAST COMMON MULTIPLE OF FIRST N NUMBERS",
-    items: ["12", "60", "420", "840"],
-    level: 3,
-  },
-  {
-    category: "POWERS OF FOUR",
-    items: ["4", "16", "64", "256"],
-    level: 2,
-  }
-],
-                                             [
-  {
-    category: "SQUARE NUMBERS",
-    items: ["36", "49", "64", "81"],
-    level: 1,
-  },
-  {
-    category: "PRIME NUMBERS",
-    items: ["41", "43", "47", "53"],
-    level: 2,
-  },
-  {
-    category: "FIBONACCI NUMBERS",
-    items: ["34", "55", "89", "144"],
-    level: 3,
-  },
-  {
-    category: "CATALAN NUMBERS",
-    items: ["5", "14", "42", "132"],
-    level: 4,
-  },
-],
-                                             
-[
-  {
-    category: "SQUARE NUMBERS",
-    items: ["1", "4", "9", "16"],
-    level: 1,
-  },
-  {
-    category: "DIVISIBLE BY 2 BUT NOT DIVISIBLE BY 3",
-    items: ["2", "8", "10", "14"],
-    level: 2,
-  },
-  {
-    category: "DIVISIBLE BY 3",
-    items: ["3", "6", "12", "15"],
-    level: 3,
-  },
-  {
-    category: "PRIME NUMBERS",
-    items: ["5", "7", "11", "13"],
-    level: 4,
-  },
-],
-[
-  {
-    category: "SQUARE NUMBERS",
-    items: ["100", "10000", "4", "1"],
-    level: 1,
-  },
-  {
-    category: "PRIME NUMBERS",
-    items: ["11", "31", "41", "5"],
-    level: 2,
-  },
-  {
-    category: "DIGITS SUM TO 3",
-    items: ["21", "30", "12", "3"],
-    level: 3,
-  },
-  {
-    category: "MULTIPLES OF 5",
-    items: ["25", "10", "125", "625"],
-    level: 4,
-  },
-],
+    {
+      category: "NUMBERS READING THE SAME UPSIDE DOWN",
+      items: ["11", "69", "96", "888"],
+      level: 1,
+    },
+    {
+      category: "CENTRAL BINOMIAL COEFFICIENTS",
+      items: ["20", "70", "252", "924"],
+      level: 4,
+    },
+    {
+      category: "LEAST COMMON MULTIPLE OF FIRST N NUMBERS",
+      items: ["12", "60", "420", "840"],
+      level: 3,
+    },
+    {
+      category: "POWERS OF FOUR",
+      items: ["4", "16", "64", "256"],
+      level: 2,
+    }
+  ],
 ];
